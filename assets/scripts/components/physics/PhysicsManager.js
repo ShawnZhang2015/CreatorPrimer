d6f73504bff4cb04c30eec53eac0f36c7bae4a92
zxh <zxh1982@qq.com>
2018-09-30 21:08:31 +0800

    更新PhysicsManager.js调试开关仅在预览模式有效

--- assets/scripts/components/physics/PhysicsManager.js
+++ assets/scripts/components/physics/PhysicsManager.js
@@ -1,7 +1,7 @@
 
 
 /**
- * 物理引擎管理组件，开启各种重调试
+ * 物理引擎管理组件，开启各种调试
  */
 cc.Class({
     extends: cc.Component,
@@ -25,7 +25,7 @@ cc.Class({
        },
        joint: { 
            default: true,
-           tooltip: '是否显示连接线'
+           tooltip: '是否显示关节连接线'
        },
        shape: {
            default: true,
@@ -37,7 +37,7 @@ cc.Class({
        }
     },
 
-    onLoad () {
+    onEnable() {
         //开启或关闭物理系统
         let physicsManager = cc.director.getPhysicsManager();
         if (physicsManager.enabled && this.active) {
@@ -50,24 +50,42 @@ cc.Class({
         }
 
         //设置调试标志
-        let DrawBits = cc.PhysicsManager.DrawBits;
-        physicsManager.debugDrawFlags = 
-            (this.aabb && DrawBits.e_aabbBit) |
-            (this.pair && DrawBits.e_pairBit) |
-            (this.centerOfMass && DrawBits.e_centerOfMassBit) |
-            (this.joint && DrawBits.e_jointBit) |
-            (this.shape && DrawBits.e_shapeBit);
+        let drawBits = cc.PhysicsManager.DrawBits;
+        if (CC_PREVIEW) {
+            physicsManager.debugDrawFlags = 
+            (this.aabb && drawBits.e_aabbBit) |
+            (this.pair && drawBits.e_pairBit) |
+            (this.centerOfMass && drawBits.e_centerOfMassBit) |
+            (this.joint && drawBits.e_jointBit) |
+            (this.shape && drawBits.e_shapeBit);
+        } else {
+            physicsManager.debugDrawFlags = 0;
+        }
+       
+
+        this._setMouseJoint();    
     },
 
-    start() {
+    _setMouseJoint() {
         //鼠标可拖刚体
         if(this.mouseJoint && this.active) {
             let node = this.node;
-            let rigidBody = node.addComponent(cc.RigidBody);
-            let mouseJoint = node.addComponent(cc.MouseJoint);
+            //获取节点上的刚体组件
+            let rigidBody = node.getComponent(cc.RigidBody);
+            //不存在添加一个
+            if (!rigidBody) {
+                rigidBody = node.addComponent(cc.RigidBody);
+            }
+            //获取组件上的鼠标关节组件
+            let mouseJoint = node.getComponent(cc.MouseJoint);
+            //不存在添加一个
+            if (!mouseJoint) {
+                mouseJoint = node.addComponent(cc.MouseJoint);
+            }
+            //设置为静态刚体
             rigidBody.type = cc.RigidBodyType.Static;
+            //设置鼠标范围
             mouseJoint.mouseRegion = node;
-            //node.parent = this.node;
         }
     },
 

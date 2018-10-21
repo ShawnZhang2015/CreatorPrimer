90af931192a0e552435c9de32598a0e77f21ea88
zxh <zxh1982@qq.com>
2018-10-21 14:59:39 +0800

    增加通用Shader组件，以及部分Shader程序

---
+++ assets/scripts/components/Shader.js
@@ -0,0 +1,78 @@
+let CustomMaterial = require("CustomMaterial");
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        _shaderObject: null,
+        shaderFile: {
+            default: '',
+            notify() {
+                if (!this.shaderFile) {
+                    return;
+                }
+                this.applyShader();
+            }
+        },
+    },
+
+    onLoad() {
+        this.sprite = this.getComponent(cc.Sprite);
+        this.applyShader();
+    },
+
+    update() {
+        if (this._shaderObject.update) {
+            this._shaderObject.update(this.sprite, this.material);
+        }
+    },
+
+    /**
+     * 启用Shader
+     */
+    applyShader() {
+        if (CC_EDITOR) {
+            return;
+        }
+
+        this._shaderObject = require(this.shaderFile);
+        if (!this._shaderObject) {
+            return;
+        }
+        CustomMaterial.addShader(this._shaderObject);
+        let sprite = this.sprite;
+        let params = this._shaderObject.params;
+        let defines = this._shaderObject.defines;
+        let material = sprite.getMaterial(this._shaderObject.name);
+        
+        if (!material) {
+            material = new CustomMaterial(this._shaderObject.name, params, defines || []);
+            sprite.setMaterial(this._shaderObject.name, material);
+        }
+        this.material = material;
+
+        sprite.activateMaterial(this._shaderObject.name);
+
+        //设置Shader参数初值
+        if (params) {
+            params.forEach((item) => {
+                if (item.defaultValue !== undefined) {
+                    material.setParamValue(item.name, item.defaultValue);
+                }
+            });
+        }
+
+        if (this._shaderObject.start) {
+            this._shaderObject.start(sprite, material);
+        }
+    },
+
+    /**
+     * 重置
+     */
+    reset() {
+        this.node.color = cc.Color.WHITE;
+        let sprite = this.getComponent(cc.Sprite);
+        sprite.spriteFrame.getTexture().update({flipY: false});
+    },
+});
